00befd629de37f367c45affde51f509ffd624046
EnayetHossain <enayethossasin135@gmail.com>
2023-10-19 20:51:37 +0600

    jwt token integration

--- src/Components/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Components/Pages/Home/PopularClass/PopularClass.jsx
@@ -18,7 +18,7 @@ const PopularClass = () => {
       </h1>
       <div className="max-w-screen-xl mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {classes.map((cls) => (
-          <ClassCard key={cls} cls={cls}></ClassCard>
+          <ClassCard key={cls._id} cls={cls}></ClassCard>
         ))}
       </div>
     </section>
